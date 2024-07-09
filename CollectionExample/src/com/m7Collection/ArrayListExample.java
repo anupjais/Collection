20e8af0981da1e92dfd7bad246abe08aa7f50431
Anup Jaiswal <150141325+anupjais@users.noreply.github.com>
2024-07-09 09:56:47 +0530

    Collection with Eclipse

---
+++ CollectionExample/src/com/m7Collection/ArrayListExample.java
@@ -0,0 +1,12 @@
+package com.m7Collection;
+
+public class ArrayListExample
+{
+	public static void main(String[] args)
+	{
+//		Collection col = Arrays.asList(19,10,20,30);
+//		System.out.println(col);
+		System.out.println("Hello There!");
+	}
+
+}
---
+++ CollectionExampleM7/src/com/m7Collection/ArrayListExample.java
@@ -0,0 +1,14 @@
+package com.m7Collection;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class ArrayListExample
+{
+	public static void main(String[] args)
+	{
+		Collection col = Arrays.asList(19,10,20,40);
+		System.out.println(col);
+	}
+
+}

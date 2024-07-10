84a383ec93ccd9d9c00c206a9b32b75183ee2d85
Anup Jaiswal <150141325+anupjais@users.noreply.github.com>
2024-07-10 07:28:20 +0530

    Basic

--- CollectionExampleM7/src/com/m7Collection/ArrayListExample.java
+++ CollectionExampleM7/src/com/m7Collection/ArrayListExample.java
@@ -1,14 +1,20 @@
 package com.m7Collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class ArrayListExample
 {
 	public static void main(String[] args)
 	{
 		Collection col = Arrays.asList(19,10,20,40);
-		System.out.println(col);
+		List<Integer> list = new ArrayList<>(col);
+
+		System.out.println(list);
+		list.set(2,30);
+		System.out.println(list);
 	}
 
 }
